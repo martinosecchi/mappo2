dfbf7e891d43feb65c08f08c905a738ce5bed7bb
martinosecchi <smart91k@gmail.com>
2014-07-24 15:41:17 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mappo2::Application.config.secret_token = '3b719a5138f71df39aecf706f09c90bacb5c2ab639155921a86ba1633371310879c1a052a5216a828bcdd4e1ae156ead73c4222f1f4e088ba2c300f9761ecd6f'
